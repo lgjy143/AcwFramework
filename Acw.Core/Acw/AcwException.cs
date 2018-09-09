7d7ce35bd9237673974564c536c02069c1e86a67
u724 <lgjy143@163.com>
2018-09-10 04:11:12 +0800

    module

---
+++ Acw.Core/Acw/AcwException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Acw.Core.Acw
+{
+    public class AcwException : Exception
+    {
+        /// <summary>
+        /// Creates a new <see cref="AcwException"/> object.
+        /// </summary>
+        public AcwException()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AcwException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public AcwException(string message)
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AcwException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Inner exception</param>
+        public AcwException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor for serializing.
+        /// </summary>
+        public AcwException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context)
+        {
+
+        }
+    }
+}
---
+++ Acw.Core/Acw/Modularity/ModuleLoader.cs
@@ -0,0 +1,132 @@
+﻿using Acw.Core.Acw.Modularity.PlugIns;
+using Acw.Core.System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Acw.Core.Acw.Modularity
+{
+    public class ModuleLoader : IModuleLoader
+    {
+        public IAcwModuleDescriptor[] LoadModules(
+         IServiceCollection services,
+         Type startupModuleType,
+         PlugInSourceList plugInSources)
+        {
+            Check.NotNull(services, nameof(services));
+            Check.NotNull(startupModuleType, nameof(startupModuleType));
+            Check.NotNull(plugInSources, nameof(plugInSources));
+
+            var modules = GetDescriptors(services, startupModuleType, plugInSources);
+
+            modules = SortByDependency(modules, startupModuleType);
+            ConfigureServices(modules, services);
+
+            return modules.ToArray();
+        }
+
+        private List<IAcwModuleDescriptor> GetDescriptors(
+          IServiceCollection services,
+          Type startupModuleType,
+          PlugInSourceList plugInSources)
+        {
+            var modules = new List<AcwModuleDescriptor>();
+
+            FillModules(modules, services, startupModuleType, plugInSources);
+            SetDependencies(modules);
+
+            return modules.Cast<IAcwModuleDescriptor>().ToList();
+        }
+
+        protected virtual void FillModules(
+            List<AcwModuleDescriptor> modules,
+            IServiceCollection services,
+            Type startupModuleType,
+            PlugInSourceList plugInSources)
+        {
+            //All modules starting from the startup module
+            foreach (var moduleType in AcwModuleHelper.FindAllModuleTypes(startupModuleType))
+            {
+                modules.Add(CreateModuleDescriptor(services, moduleType));
+            }
+
+            //Plugin modules
+            foreach (var moduleType in plugInSources.GetAllModules())
+            {
+                if (modules.Any(m => m.Type == moduleType))
+                {
+                    continue;
+                }
+
+                modules.Add(CreateModuleDescriptor(services, moduleType, isLoadedAsPlugIn: true));
+            }
+        }
+
+        protected virtual void SetDependencies(List<AcwModuleDescriptor> modules)
+        {
+            foreach (var module in modules)
+            {
+                SetDependencies(modules, module);
+            }
+        }
+
+        protected virtual List<IAcwModuleDescriptor> SortByDependency(List<IAcwModuleDescriptor> modules, Type startupModuleType)
+        {
+            var sortedModules = modules.SortByDependencies(m => m.Dependencies);
+            sortedModules.MoveItem(m => m.Type == startupModuleType, modules.Count - 1);
+            return sortedModules;
+        }
+
+        protected virtual AcwModuleDescriptor CreateModuleDescriptor(IServiceCollection services, Type moduleType, bool isLoadedAsPlugIn = false)
+        {
+            return new AcwModuleDescriptor(moduleType, CreateAndRegisterModule(services, moduleType), isLoadedAsPlugIn);
+        }
+
+        protected virtual IAcwModule CreateAndRegisterModule(IServiceCollection services, Type moduleType)
+        {
+            var module = (IAcwModule)Activator.CreateInstance(moduleType);
+            services.AddSingleton(moduleType, module);
+            return module;
+        }
+
+        protected virtual void ConfigureServices(List<IAcwModuleDescriptor> modules, IServiceCollection services)
+        {
+            var context = new ServiceConfigurationContext(services);
+            services.AddSingleton(context);
+
+            //PreConfigureServices
+            foreach (var module in modules.Where(m => m.Instance is IPreConfigureServices))
+            {
+                ((IPreConfigureServices)module.Instance).PreConfigureServices(context);
+            }
+
+            //ConfigureServices
+            foreach (var module in modules)
+            {
+                module.Instance.ConfigureServices(context);
+            }
+
+            //PostConfigureServices
+            foreach (var module in modules.Where(m => m.Instance is IPostConfigureServices))
+            {
+                ((IPostConfigureServices)module.Instance).PostConfigureServices(context);
+            }
+        }
+
+        protected virtual void SetDependencies(List<AcwModuleDescriptor> modules, AcwModuleDescriptor module)
+        {
+            foreach (var dependedModuleType in AcwModuleHelper.FindDependedModuleTypes(module.Type))
+            {
+                var dependedModule = modules.FirstOrDefault(m => m.Type == dependedModuleType);
+                if (dependedModule == null)
+                {
+                    throw new AcwException("Could not find a depended module " + dependedModuleType.AssemblyQualifiedName + " for " + module.Type.AssemblyQualifiedName);
+                }
+
+                module.AddDependency(dependedModule);
+            }
+        }
+    }
+}
